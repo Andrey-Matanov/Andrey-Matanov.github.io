129fc5ae6fd2f8365f39c8f0f168bec4f04ec286
Andrew <rtrebor608@gmail.com>
2020-10-10 00:14:20 +0500

    added_online_shop_to_my_projects

---
+++ my_projects/online_shop/components/common.js
@@ -0,0 +1,15 @@
+let to_top_button = document.querySelector('.to_top_button');
+
+const onScrollFunction = () => {
+    if (window.pageYOffset > 150) {
+        to_top_button.style.opacity = '1'
+    } else {
+        to_top_button.style.opacity = '0'
+    }
+}
+
+to_top_button.onclick = function () {
+    window.scrollTo(0, 0)
+}
+
+window.onscroll = onScrollFunction;
